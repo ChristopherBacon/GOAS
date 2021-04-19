743536eea711da9d7a215468587d35c22d4b1a01
ChristopherBacon <chrisjbacon85@gmail.com>
2021-04-19 08:17:22 +0100

    grid for blog etc.

--- home/models.py
+++ home/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
-from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
 
+from wagtail.core.models import Page
+from wagtail.core import blocks
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
+from blog.blocks import TwoColumnBlock
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    header = RichTextField(blank=True, null=True)
+    body = StreamField([('heading', blocks.CharBlock(from_classname="full title")),
+                         ('paragraph', blocks.RichTextBlock()),
+                         ('image', ImageChooserBlock(icon="image")),
+                         ('two_columns', TwoColumnBlock()),
+                         ('embedded_video', EmbedBlock(icon="media"))], null=True, blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        FieldPanel('header', classname="full"),
+        StreamFieldPanel('body', classname="full")
     ]
