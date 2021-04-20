efbd37884e453f8b2080ea096385429a3b48f39f
ChristopherBacon <chrisjbacon85@gmail.com>
2021-04-20 09:40:39 +0100

    two-column removal

--- home/models.py
+++ home/models.py
@@ -1,23 +1,21 @@
+
+
+
 from django.db import models
-
-
 from wagtail.core.models import Page
 from wagtail.core import blocks
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.embeds.blocks import EmbedBlock
 from wagtail.images.blocks import ImageChooserBlock
-from blog.blocks import TwoColumnBlock
+
 
 class HomePage(Page):
     header = RichTextField(blank=True, null=True)
-    body = StreamField([('heading', blocks.CharBlock(from_classname="full title")),
-                         ('paragraph', blocks.RichTextBlock()),
-                         ('image', ImageChooserBlock(icon="image")),
-                         ('two_columns', TwoColumnBlock()),
-                         ('embedded_video', EmbedBlock(icon="media"))], null=True, blank=True)
+    body = models.TextField(blank=True, null=True)
+
 
     content_panels = Page.content_panels + [
         FieldPanel('header', classname="full"),
-        StreamFieldPanel('body', classname="full")
+        FieldPanel('body', classname="full")
     ]
